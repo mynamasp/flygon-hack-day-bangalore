164e54664201c9617de9180d91771674d7c49772
Surendar PD <80924716+surendar-pd@users.noreply.github.com>
2022-11-06 16:36:22 +0530

    give and take page

--- pages/services.tsx
+++ pages/services.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
 import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import ChevronLeftOutlinedIcon from '@mui/icons-material/ChevronLeftOutlined';
 
 function services() {
 
-    const [service, setService] = useState('give')
+    const [service, setService] = useState('')
 
     return (
         <div className="w-full h-screen flex flex-col justify-center items-center gap-4">
@@ -22,15 +24,45 @@ function services() {
                         </div>
                     </>
                 : service === "give" ?
-                <div>
-                    <h1 className="text-4xl text-center font-semibold text-white">Give</h1>
-                        <Paper className="flex p-8 flex-col gap-8 mt-4">
-                                <TextField id="outlined-basic" label="Weight Occupied" variant="outlined" />
-                                <h1 className="text-center text-green-500">ETH number</h1>
-                        </Paper>
+                <div className="w-1/2">
+                    <div className="flex items-center">
+                        <ChevronLeftOutlinedIcon onClick={() => setService("")} fontSize="large" className="cursor-pointer text-white mr-4 mt-1"/>
+                        <h1 className="text-4xl text-center font-semibold text-white">Give</h1>
+                    </div>
+                    <Paper className="flex p-8 flex-col gap-8 mt-4">
+                            <TextField id="outlined-basic" label="Weight Occupied" variant="outlined" />
+                            <Button variant="text"><h1 className="text-center text-green-500">Recieve 14 MATIC</h1></Button>
+                    </Paper>
                 </div>
                 : 
-                <div></div>
+                <div className="w-1/2">
+                    <div className="flex items-center">
+                        <ChevronLeftOutlinedIcon onClick={() => setService("")} fontSize="large" className="cursor-pointer text-white mr-4 mt-1"/>
+                        <h1 className="text-4xl text-center font-semibold text-white">Take</h1>
+                    </div>
+                    <div className="max-h-[40rem] overflow-y-scroll">
+                        <Paper className="flex p-8 justify-between mt-4 cursor-pointer">
+                            <h1 className="text-xl font-semibold">6Kg</h1>
+                            <h1 className="text-xl text-red-500">14 MATIC</h1>
+                            <Button variant="text" className="text-green-500">Accept</Button>
+                        </Paper>
+                        <Paper className="flex p-8 justify-between mt-4 cursor-pointer">
+                            <h1 className="text-xl font-semibold">6Kg</h1>
+                            <h1 className="text-xl text-red-500">14 MATIC</h1>
+                            <Button variant="text" className="text-green-500">Accept</Button>
+                        </Paper>
+                        <Paper className="flex p-8 justify-between mt-4 cursor-pointer">
+                            <h1 className="text-xl font-semibold">6Kg</h1>
+                            <h1 className="text-xl text-red-500">14 MATIC</h1>
+                            <Button variant="text" className="text-green-500">Accept</Button>
+                        </Paper>
+                        <Paper className="flex p-8 justify-between mt-4 cursor-pointer">
+                            <h1 className="text-xl font-semibold">6Kg</h1>
+                            <h1 className="text-xl text-red-500">14 MATIC</h1>
+                            <Button variant="text" className="text-green-500">Accept</Button>
+                        </Paper>
+                    </div>
+                </div>
             }
         </div>
     )
